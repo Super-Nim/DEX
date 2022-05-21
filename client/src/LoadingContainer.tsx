85c31bfb0f5477dcf6aa6d7b1ccd69020fa9e54d
Hakeem <64858288+Super-Nim@users.noreply.github.com>
2022-05-21 07:30:28 +0100

    empty

--- client/src/LoadingContainer.tsx
+++ client/src/LoadingContainer.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getWeb3, getContracts } from './utils.js';
 import App from './App.js';
 
-function LoadingContainer() {
+function LoadingContainer() {   
     const [web3, setWeb3] = useState(undefined);
     const [accounts, setAccounts] = useState([]);
     const [contracts, setContracts] = useState(undefined);
