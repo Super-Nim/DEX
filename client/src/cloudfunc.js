0e477d45950152198dcb850d456530bac91aab78
Hakeem <64858288+Super-Nim@users.noreply.github.com>
2022-05-23 07:29:36 +0100

    added yarn

--- client/src/cloudfunc.js
+++ client/src/cloudfunc.js
@@ -1,12 +1,11 @@
 Moralis.Cloud.define("getPrice", async (request) => {
-    let url = "https://deep-index.moralis.io/api/v2/token/ERC20/" + request.params.address + "/price"
+    let url = "https://deep-index.moralis.io/api/v2/erc20/" + request.params.address + "/price?chain=eth"
     logger.info(url);
     return Moralis.Cloud.httpRequest({
       url: url,
       params: {chain: "eth", chain_name: "ropsten"},
       headers: {
         "accept": "application/json",
-        "X-API-Key": "viQQvEfmTzgOHEDSuJ7UKj7UVTJ6pPT8OJSzk9EWhR3C3T3Bic2EVLFqhrZhGCpK"
       }
     }).then( function(httpResponse){
       return httpResponse.data;
